231b57b08adaf7cae8162f665a6d7220bc9cd43e
Rahul Shyam <rahulcvfiitjee@gmail.com>
2026-04-11 11:05:40 +0530

    feat: Add comprehensive event data and constants
    Introduces a new set of constants for various international and awareness days, enhancing the application's data richness. Updates the `src/data.ts` file to reflect the new constants and ensures data consistency. This change expands the available event information for users.

---
+++ src/constants.ts
@@ -0,0 +1,136 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const EVENT_CATEGORIES = [
+  // January
+  "World Braille Day",           // Jan 4
+  "World Logic Day",             // Jan 14
+  "World Religion Day",          // Jan 15
+  "International Day of Education", // Jan 24
+  "International Holocaust Remembrance Day", // Jan 27
+  // February
+  "World Wetlands Day",          // Feb 2
+  "World Cancer Day",            // Feb 4
+  "Safer Internet Day",          // Feb 11
+  "World Radio Day",             // Feb 13
+  "World Mother Language Day",   // Feb 21
+  "World Engineering Day",       // Feb 23
+  // March
+  "World Wildlife Day",          // Mar 3
+  "World Hearing Day",           // Mar 3
+  "International Women's Day",   // Mar 8
+  "World Kidney Day",            // Mar 10
+  "World Consumer Rights Day",   // Mar 15
+  "International Day of Happiness", // Mar 20
+  "World Water Day",             // Mar 22
+  "World Meteorological Day",    // Mar 23
+  "Earth Hour",                  // Mar last Saturday
+  "World TB Day",                // Mar 24
+  "World Theatre Day",           // Mar 27
+  // April
+  "World Autism Awareness Day",  // Apr 2
+  "World Health Day",            // Apr 7
+  "World Homeopathy Day",        // Apr 10
+  "World Art Day",               // Apr 15
+  "World Book Day",              // Apr 23
+  "World Immunization Week",     // Last week of April
+  "World Malaria Day",           // Apr 25
+  "World Intellectual Property Day", // Apr 26
+  // May
+  "World Press Freedom Day",     // May 3
+  "World Thalassaemia Day",      // May 8
+  "World Migratory Bird Day",    // May 11
+  "International Nurses Day",    // May 12
+  "World Telecommunication Day", // May 17
+  "World Hypertension Day",      // May 17
+  "World AIDS Vaccine Day",      // May 18
+  "International Museum Day",    // May 18
+  "World Bee Day",               // May 20
+  "World Measurement Day (World Metrology Day)", // May 20
+  "World Turtle Day",            // May 23
+  "World No Tobacco Day",        // May 31
+  // June
+  "World Environment Day",       // Jun 5
+  "World Ocean Day",             // Jun 8
+  "World Blood Donor Day",       // Jun 14
+  "World Wind Day",              // Jun 15
+  "World Day to Combat Desertification and Drought", // Jun 17
+  "International Day of Yoga",   // Jun 21
+  "World Rainforest Day",        // Jun 22
+  "International Day of the Tropics", // Jun 29
+  // July
+  "World Population Day",        // Jul 11
+  "World Youth Skills Day",      // Jul 15
+  "Nelson Mandela International Day", // Jul 18
+  "World Chess Day",             // Jul 20
+  "World Hepatitis Day",         // Jul 28
+  "International Day of Friendship", // Jul 30
+  // August
+  "World Wide Web Day",          // Aug 1
+  "International Youth Day",     // Aug 12
+  "World Elephant Day",          // Aug 12
+  "World Humanitarian Day",      // Aug 19
+  "World Photography Day",       // Aug 19
+  "World Mosquito Day",          // Aug 20
+  "World Senior Citizen Day",    // Aug 21
+  "International Day for the Remembrance of the Slave Trade", // Aug 23
+  "World Water Week",            // Late August
+  // September
+  "World Coconut Day",           // Sep 2
+  "World Suicide Prevention Day",// Sep 10
+  "International Day of Democracy", // Sep 15
+  "International Day for the Preservation of the Ozone Layer", // Sep 16
+  "World Cleanup Day",           // Sep 16
+  "International Day of Peace",  // Sep 21
+  "World Alzheimers Day",        // Sep 21
+  "World Rivers Day",            // Sep 25
+  "World Heart Day",             // Sep 29
+  "World Tourism Day",           // Sep 27
+  "International Translation Day", // Sep 30
+  // October
+  "World Habitat Day",           // Oct 1
+  "World Animal Day",            // Oct 4
+  "World Teachers Day",          // Oct 5
+  "World Cerebral Palsy Day",    // Oct 6
+  "World Mental Health Day",     // Oct 10
+  "International Day of the Girl Child", // Oct 11
+  "World Egg Day",               // Oct 14
+  "World Standards Day",         // Oct 14
+  "World Food Day",              // Oct 16
+  "World Statistics Day",        // Oct 20
+  "World Development Information Day", // Oct 24
+  "World Stroke Day",            // Oct 29
+  "World Cities Day",            // Oct 31
+  // November
+  "World Tsunami Awareness Day", // Nov 5
+  "International Day for Preventing Exploitation of the Environment in War", // Nov 6
+  "World Science Day for Peace and Development", // Nov 10
+  "World Pneumonia Day",         // Nov 12
+  "World Kindness Day",          // Nov 13
+  "International Day for Tolerance", // Nov 16
+  "World Diabetes Day",          // Nov 14
+  "World Philosophy Day",        // Nov 17
+  "World Children's Day",        // Nov 20
+  "Universal Children's Day",    // Nov 20
+  "World Television Day",        // Nov 21
+  "World Soil Day",              // Dec 5
+  "World AIDS Day",              // Dec 1
+  "International Day for the Abolition of Slavery", // Dec 2
+  "International Day of Persons with Disabilities", // Dec 3
+  "International Human Rights Day", // Dec 10
+  "World Energy Conservation Day", // Dec 14
+  // Additional Engineering & Tech Days
+  "World Quantum Day",           // Apr 14
+  "World Computer Science Day",  // Apr 7 alternate
+  "International Open Access Day", // Oct 24
+  "World Usability Day",         // 2nd Thursday of November
+  "Ada Lovelace Day",            // 2nd Tuesday of October
+  "World Standards Week",        // October
+  "National Technology Day",     // May 11 (India)
+  "National Science Day",        // Feb 28 (India)
+  "Engineers Day",               // Sep 15 (India - M. Visvesvaraya)
+  "National Mathematics Day",    // Dec 22 (India - Ramanujan)
+  "National Statistics Day",     // Jun 29 (India)
+];
